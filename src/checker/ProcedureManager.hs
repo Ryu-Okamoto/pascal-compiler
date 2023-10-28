20dd4a59bea79a1aab97d24baecabe509911cbe0
Ryu-Okamoto <u514203b@ecs.osaka-u.ac.jp>
2023-10-28 19:15:55 +0900

    implemented ProcedureManager

--- src/checker/ProcedureManager.hs
+++ src/checker/ProcedureManager.hs
@@ -1,18 +1,140 @@
-module Src.Checker.ProcedureManager where
+module Src.Checker.ProcedureManager ( ProcedureName, ProcedureTable, ProcedureInfo (..), constructProcedureTable ) where
 
 import Data.Map.Strict as M
-import Data.Set as S
 
+import Src.Synonym ( LineNumber )
+import Src.Token ( Token (..) )
 import Src.AST
-import Src.Checker.DType ( DType )
+import Src.Checker.DType ( DType (..) )
 import Src.Checker.Checker ( Check (..) )
 
-data Procedure = Procedure {
+data ProcedureInfo = ProcedureInfo {
     getProcArgTypes :: [DType],
-    getProcName :: String
-}
+    getProcDefinedLine :: LineNumber
+} deriving ( Show )
 
-type ProcedureTable = Set Procedure
+type ProcedureName = String
+type ProcedureTable = Map ProcedureName ProcedureInfo
+
+{-
+    実装方針：
+     - AST 要素のうち必要なとこだけを visit
+       - 重複宣言がある場合はその行番号とともに SemanticError
+       - そうでない場合は (ProcedureName, ProcedureInfo) の Map を返す
+-}
 
 constructProcedureTable :: AST -> Check ProcedureTable
-constructProcedureTable = undefined
+constructProcedureTable = visitProgram
+
+visitProgram :: AProgram -> Check ProcedureTable
+visitProgram (
+        AProgram
+            _
+            block
+            _
+    ) = visitBlock block
+
+visitBlock :: ABlock -> Check ProcedureTable
+visitBlock (
+        ABlock
+            _
+            subprogramDeclarations
+    ) = visitSubprogramDeclarations subprogramDeclarations
+
+visitSubprogramDeclarations :: ASubprogramDeclarations -> Check ProcedureTable
+visitSubprogramDeclarations (
+        ASubprogramDeclarations
+            subprogramDeclarationList
+    ) = createProcedureTable subprogramDeclarationList
+    where
+        createProcedureTable :: [ASubprogramDeclaration] -> Check ProcedureTable
+        createProcedureTable [] = return M.empty
+        createProcedureTable (h:t) = do
+            let newTable = visitSubprogramDeclaration h
+            createdTable <- createProcedureTable t
+            let duplicatingTable = createdTable `M.intersection` newTable
+            if M.null duplicatingTable
+            then 
+                return $ newTable `M.union` createdTable
+            else do
+                let duplicating = M.elemAt 0 duplicatingTable
+                SemanticError $ getProcDefinedLine $ snd duplicating
+            
+
+visitSubprogramDeclaration :: ASubprogramDeclaration -> ProcedureTable
+visitSubprogramDeclaration (
+        ASubprogramDeclaration
+            subprogramHead
+            _
+            _
+    ) = visitSubprogramHead subprogramHead
+
+visitSubprogramHead :: ASubprogramHead -> ProcedureTable
+visitSubprogramHead (
+        ASubprogramHead
+            procedureName
+            parameter
+    ) = M.fromList [(name, info)]
+    where
+        (name, lineNumber) = visitProcedureName procedureName
+        argTypes = visitParameter parameter
+        info = ProcedureInfo argTypes lineNumber
+
+visitProcedureName :: AProcedureName -> (ProcedureName, LineNumber)
+visitProcedureName (
+    AProcedureName
+        identifier
+    ) = visitIdentifier identifier
+
+visitIdentifier :: AIdentifier -> (ProcedureName, LineNumber)
+visitIdentifier (
+        AIdentifier
+            token
+    ) = (getSSymbol token, getSLineNumber token)
+
+visitParameter :: AParameter -> [DType]
+visitParameter (
+        AParameter
+            Nothing
+    ) = []
+visitParameter (
+        AParameter
+            (Just parameterSequence)
+    ) = visitParameterSequence parameterSequence
+
+visitParameterSequence :: AParameterSequence -> [DType]
+visitParameterSequence (
+        AParameterSequence
+            parameterSequence'
+            parameterSequence'List
+    ) = createDTypeList $ parameterSequence' : parameterSequence'List
+    where
+        createDTypeList :: [AParameterSequence'] -> [DType]
+        createDTypeList [] = []
+        createDTypeList (h:t) = replicate count dtype ++ createDTypeList t
+            where
+                (dtype, count) = visitParameterSequence' h
+        
+
+visitParameterSequence' :: AParameterSequence' -> (DType, Int)
+visitParameterSequence' (
+        AParameterSequence'
+            parameterNameSequence
+            standardType
+    ) = (visitStandardType standardType, visitParameterNameSequence parameterNameSequence)
+
+visitParameterNameSequence :: AParameterNameSequence -> Int
+visitParameterNameSequence (
+        AParameterNameSequence
+            parameterName
+            parameterNameList
+    ) = length $ parameterName : parameterNameList
+
+visitStandardType :: AStandardType -> DType
+visitStandardType (
+        AStandardType
+            token
+    ) = case getSType token of
+        "SINTEGER" -> DInteger
+        "SCHAR"    -> DChar
+        "SBOOLEAN" -> DBoolean
